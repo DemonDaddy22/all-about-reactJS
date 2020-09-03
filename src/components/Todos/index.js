823f56e837940cc0cdbe925d2e58963c7c05e244
Rohan Gupta <rohan.gupta2@oyorooms.com>
2020-09-03 23:06:16 +0530

    add radio component

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -3,13 +3,15 @@ import './index.css';
 import Input from '../../ui-components/Input';
 import Button from '../../ui-components/Button';
 import CheckBox from '../../ui-components/CheckBox';
+import Radio from '../../ui-components/Radio';
 
 export default class Todos extends React.Component {
 
     state = {
         todos: [],
         value: '',
-        checked: false
+        checkedBox: false,
+        checkedRadio: false,
     }
 
     handleValueChange = e => {
@@ -18,7 +20,9 @@ export default class Todos extends React.Component {
 
     handleBtnClick = () => alert('Button clicked!');
 
-    toggleCheckBox = () => this.setState({ checked: !this.state.checked });
+    toggleCheckBox = () => this.setState({ checkedBox: !this.state.checkedBox });
+    
+    checkRadio = () => this.setState({ checkedRadio: true });
 
     render = () => {
         const { todos } = this.state;
@@ -28,7 +32,8 @@ export default class Todos extends React.Component {
             <Input id='input-test' label='test' onChange={this.handleValueChange} value={this.state.value}
                 placeholder='test' variant='outlined' />
             <Button className='btn' onClick={this.handleBtnClick}>Test</Button>
-            <CheckBox labelProps={{ style: { display: 'block', marginTop: 20 } }} label='Test' checked={this.state.checked} onChange={this.toggleCheckBox}></CheckBox>
+            <CheckBox labelProps={{ style: { display: 'block', marginTop: 20 } }} label='Test' checked={this.state.checkedBox} onChange={this.toggleCheckBox}></CheckBox>
+            <Radio labelProps={{ style: { display: 'block', marginTop: 20 } }} label='Test' checked={this.state.checkedRadio} onChange={this.checkRadio}></Radio>
         </div>;
     }
 }
---
+++ src/ui-components/Radio/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { FormControlLabel, withStyles } from '@material-ui/core';
+import Radio from '@material-ui/core/Radio';
+import { THEME_COLOR } from '../../resources/colors';
+
+const styles = {
+    root: props => ({
+        height: props.height ? props.height : 16,
+        width: props.width ? props.width : 16,
+        color: props.color ? props.color : THEME_COLOR,
+        '&$checked': {
+            color: props.color ? props.color : THEME_COLOR
+        },
+        ...props.radioStyles
+    })
+};
+
+class RadioButton extends React.Component {
+
+    render = () => {
+        const { checked, disabled, label, labelProps } = this.props;
+
+        return <FormControlLabel label={label} control={
+            <Radio color='default' checked={checked} disabled={disabled} {...this.props} />
+        } {...labelProps}></FormControlLabel>
+    }
+}
+
+export default withStyles(styles)(RadioButton);
