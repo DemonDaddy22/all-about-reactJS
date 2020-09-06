27bc6f742f24ccf29ab58e43879be74c080af5fd
Rohan Gupta <guptarohan369@gmail.com>
2020-09-06 13:37:01 +0530

    refactor styling of individual todo

--- src/components/Todos/components/Todo/index.js
+++ src/components/Todos/components/Todo/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import classes from './styles.module.css';
 import CheckBox from '../../../../ui-components/CheckBox';
+import Iconbutton from '../../../../ui-components/Button/Iconbutton';
+import EditRoundedIcon from '@material-ui/icons/EditRounded';
+import DeleteOutlineRoundedIcon from '@material-ui/icons/DeleteOutlineRounded';
 
 export default class Todo extends React.Component {
 
@@ -10,9 +13,11 @@ export default class Todo extends React.Component {
         const { value, checked, isEditable } = todo;
 
         return <div className={classes.todo}>
-            <CheckBox labelProps={{ style: { display: 'block', margin: 0 } }} checked={checked}
+            <CheckBox labelProps={{ style: { display: 'inherit', margin: 0 } }} checked={checked}
                 onChange={() => this.props.toggleCheckBox(index)}></CheckBox>
             <div className={classes.label}>{value}</div>
+            <Iconbutton className={classes.iconBtn} onClick={this.handleAddTodo} icon={<EditRoundedIcon fontSize='default' />}></Iconbutton>
+            <Iconbutton className={classes.iconBtn} onClick={this.handleAddTodo} icon={<DeleteOutlineRoundedIcon fontSize='default' />}></Iconbutton>
             {/* checkbox */}
             {/* input/text based on isEditable */}
             {/* edit icon button */}
