26cbe1ebddc2e815bf7408202043d75edb868691
Rohan Gupta <guptarohan369@gmail.com>
2020-09-06 19:02:13 +0530

    add strikethrough style to todo label

--- src/components/Todos/components/Todo/index.js
+++ src/components/Todos/components/Todo/index.js
@@ -5,6 +5,7 @@ import CheckBox from '../../../../ui-components/CheckBox';
 import Iconbutton from '../../../../ui-components/Button/Iconbutton';
 import EditRoundedIcon from '@material-ui/icons/EditRounded';
 import DeleteOutlineRoundedIcon from '@material-ui/icons/DeleteOutlineRounded';
+import { GREEN_700, RED_700 } from '../../../../resources/colors';
 
 export default class Todo extends React.Component {
 
@@ -15,13 +16,12 @@ export default class Todo extends React.Component {
         return <div className={classes.todo}>
             <CheckBox labelProps={{ style: { display: 'inherit', margin: 0 } }} checked={checked}
                 onChange={() => this.props.toggleCheckBox(index)}></CheckBox>
-            <div className={classes.label}>{value}</div>
-            <Iconbutton className={classes.iconBtn} onClick={this.handleAddTodo} icon={<EditRoundedIcon fontSize='default' />}></Iconbutton>
-            <Iconbutton className={classes.iconBtn} onClick={this.handleAddTodo} icon={<DeleteOutlineRoundedIcon fontSize='default' />}></Iconbutton>
-            {/* checkbox */}
+            <div className={classes.labelWrapper}>
+                <div className={`${classes.label} ${checked && classes.strike}`}>{value}</div>
+            </div>
+            <Iconbutton color={GREEN_700} className={classes.iconBtn} onClick={this.handleAddTodo} icon={<EditRoundedIcon fontSize='default' />}></Iconbutton>
+            <Iconbutton color={RED_700} className={classes.iconBtn} onClick={this.handleAddTodo} icon={<DeleteOutlineRoundedIcon fontSize='default' />}></Iconbutton>
             {/* input/text based on isEditable */}
-            {/* edit icon button */}
-            {/* delete icon button */}
         </div>;
     }
 }
