59288f0298af8eb408b9d8122fba270a8109bfce
Rohan Gupta <guptarohan369@gmail.com>
2020-09-08 22:54:06 +0530

    add basic price card styles

--- src/components/PricingCards/index.js
+++ src/components/PricingCards/index.js
@@ -16,9 +16,12 @@ export default class PricingCards extends React.Component {
 
     render = () => {
         return <div className={classes.cardsContainer}>
-            <Card cardContainerClass={classes.cardMargin} />
-            <Card cardContainerClass={classes.cardMargin} />
-            <Card cardContainerClass={classes.cardMargin} />
+            <Card title={'Premium'} price={'$10'} features={['200+ detailed resources', '100+ carefully designed exercises', '1-on-1 on-demand mentor sessions']}
+                icon={'https://svgsilh.com/svg/1614530.svg'} cardContainerClass={classes.cardMargin} />
+            <Card title={'Premium'} price={'$10'} features={['200+ detailed resources', '100+ carefully designed exercises', '1-on-1 on-demand mentor sessions']}
+                icon={'https://svgsilh.com/svg/1614530.svg'} cardContainerClass={classes.cardMargin} />
+            <Card title={'Premium'} price={'$10'} features={['200+ detailed resources', '100+ carefully designed exercises', '1-on-1 on-demand mentor sessions']}
+                icon={'https://svgsilh.com/svg/1614530.svg'} cardContainerClass={classes.cardMargin} />
         </div>
     }
 }
