0434e59421d4805fa90efd0633bb8846f428378c
Rohan Gupta <guptarohan369@gmail.com>
2020-09-08 23:04:38 +0530

    add theme.js

--- src/components/PricingCards/index.js
+++ src/components/PricingCards/index.js
@@ -3,16 +3,7 @@ import React from 'react';
 import classes from './styles.module.css';
 import Card from './components/Card';
 
-const themes = Object.freeze({
-    LIGHT: 'light',
-    DARK: 'dark'
-});
-
 export default class PricingCards extends React.Component {
-
-    state = {
-        theme: themes.LIGHT
-    }
 
     render = () => {
         return <div className={classes.cardsContainer}>
---
+++ src/components/PricingCards/theme.js
@@ -0,0 +1,34 @@
+import { getPathValue, isEmptyString } from "../../utils";
+import { createMuiTheme } from "@material-ui/core";
+import { THEME_COLOR, THEME_COLOR_DARK, TEXT_COLOR, TEXT_COLOR_DARK } from "../../resources/colors";
+
+const themes = Object.freeze({
+    LIGHT: 'light',
+    DARK: 'dark'
+});
+
+export const themed = (lightVal, darkVal) => getTheme() === 'light' ? lightVal : darkVal;
+
+export const getTheme = () => {
+    const theme = !isEmptyString(JSON.parse(localStorage.getItem('pricingTheme'))) ? JSON.parse(localStorage.getItem('pricingTheme')) : themes.LIGHT;
+    return theme;
+}
+
+export const setTheme = theme => {
+    localStorage.setItem('pricingTheme', JSON.stringify(theme));
+    document.documentElement.setAttribute('theme-mode', theme);
+}
+
+export const getMuiTheme = () => createMuiTheme({
+    palette: {
+        type: getTheme(),
+        primary: {
+            main: THEME_COLOR,
+            light: THEME_COLOR,
+            dark: THEME_COLOR_DARK
+        },
+        text: {
+            primary: themed(TEXT_COLOR, TEXT_COLOR_DARK),
+        }
+    }
+})
