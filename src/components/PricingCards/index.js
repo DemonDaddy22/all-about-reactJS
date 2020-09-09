b6fb5c5e595a75b9a64f78ff79bf238f4f83c191
Rohan Gupta <guptarohan369@gmail.com>
2020-09-09 20:35:18 +0530

    add centerCard class to center card

--- src/components/PricingCards/index.js
+++ src/components/PricingCards/index.js
@@ -7,12 +7,12 @@ export default class PricingCards extends React.Component {
 
     render = () => {
         return <div className={classes.cardsContainer}>
-            <Card title={'Premium'} price={'$10'} features={['200+ detailed resources', '100+ carefully designed exercises', '1-on-1 on-demand mentor sessions']}
-                icon={'https://svgsilh.com/svg/1614530.svg'} cardContainerClass={classes.cardMargin} />
-            <Card title={'Premium'} price={'$10'} features={['200+ detailed resources', '100+ carefully designed exercises', '1-on-1 on-demand mentor sessions']}
-                icon={'https://svgsilh.com/svg/1614530.svg'} cardContainerClass={classes.cardMargin} />
-            <Card title={'Premium'} price={'$10'} features={['200+ detailed resources', '100+ carefully designed exercises', '1-on-1 on-demand mentor sessions']}
-                icon={'https://svgsilh.com/svg/1614530.svg'} cardContainerClass={classes.cardMargin} />
+            <Card title={'Premium'} price={'$10'} cardContainerClass={classes.cardMargin}
+                features={['200+ detailed resources', '100+ carefully designed exercises', '1-on-1 on-demand mentor sessions']} />
+            <Card title={'Premium'} price={'$10'} cardContainerClass={`${classes.cardMargin} ${classes.centerCard}`}
+                features={['200+ detailed resources', '100+ carefully designed exercises', '1-on-1 on-demand mentor sessions']} />
+            <Card title={'Premium'} price={'$10'} cardContainerClass={classes.cardMargin}
+                features={['200+ detailed resources', '100+ carefully designed exercises', '1-on-1 on-demand mentor sessions']} />
         </div>
     }
 }
