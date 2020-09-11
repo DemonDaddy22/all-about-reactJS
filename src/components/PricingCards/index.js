ccbf2a6ae2a4258606a006b916ce2b2bc9309452
Rohan Gupta <guptarohan369@gmail.com>
2020-09-11 09:52:47 +0530

    store colors in state for proper switching of colors

--- src/components/PricingCards/index.js
+++ src/components/PricingCards/index.js
@@ -8,24 +8,27 @@ import Brightness4RoundedIcon from '@material-ui/icons/Brightness4Rounded';
 import Brightness7RoundedIcon from '@material-ui/icons/Brightness7Rounded';
 import { GREY_50, GREEN_500, GREEN_300, INDIGO_300, INDIGO_500 } from '../../resources/colors';
 
-// update styles of pricing card
 // use var and themed for colors for future implementation of theme on all the projects
 export default class PricingCards extends React.Component {
 
     state = {
-        theme: false
+        theme: false,
+        primaryColor: themed(INDIGO_500, INDIGO_300),
+        secondaryColor: themed(GREEN_500, GREEN_300)
     }
 
     componentDidMount = () => this.loadTheme();
 
     loadTheme = () => {
         const currTheme = getTheme();
-        this.setState({ theme: currTheme === themes.LIGHT ? false : true }, () => setTheme(currTheme));
+        this.setState({ theme: currTheme === themes.LIGHT ? false : true, primaryColor: themed(INDIGO_500, INDIGO_300), secondaryColor: themed(GREEN_500, GREEN_300) },
+            () => setTheme(currTheme));
     }
 
     handleThemeChange = () => this.setState({ theme: !this.state.theme },
         () => {
             setTheme(this.state.theme ? themes.DARK : themes.LIGHT);
+            this.setState({ primaryColor: themed(INDIGO_500, INDIGO_300), secondaryColor: themed(GREEN_500, GREEN_300) });
             // window.location.reload();
         });
 
@@ -34,15 +37,15 @@ export default class PricingCards extends React.Component {
             <Iconbutton iconColor={GREY_50} onClick={this.handleThemeChange} icon={this.state.theme ? <Brightness7RoundedIcon fontSize='large' /> : <Brightness4RoundedIcon fontSize='large' />}></Iconbutton>
         </div>
         <div className={classes.cardsContainer}>
-            <Card title={{ label: 'Basic', style: { backgroundColor: themed(INDIGO_300, INDIGO_500) } }} price={{ label: '$25', style: { color: themed(INDIGO_300, INDIGO_500) } }}
-                cardContainerClass={classes.cardMargin} features={{ features: ['Online access to resources', 'Limited quizzes & exercises', 'Mentors for doubt clearance'], style: { color: themed(INDIGO_300, INDIGO_500) } }}
-                buttonColor={themed(INDIGO_300, INDIGO_500)} />
-            <Card title={{ label: 'Premium', style: { backgroundColor: themed(GREEN_300, GREEN_500) } }} subtitle={{ label: 'Most Popular', style: { color: themed(GREEN_300, GREEN_500) } }}
-                price={{ label: '$40', style: { color: themed(GREEN_300, GREEN_500) } }} cardContainerClass={`${classes.cardMargin} ${classes.centerCard}`} buttonColor={themed(GREEN_300, GREEN_500)}
-                features={{ features: ['Downloadable resources', '100+ quizzes & exercises', '1-on-1 mentor sessions'], style: { color: themed(GREEN_300, GREEN_500) } }} />
-            <Card title={{ label: 'Ultimate', style: { backgroundColor: themed(INDIGO_300, INDIGO_500) } }} price={{ label: '$50', style: { color: themed(INDIGO_300, INDIGO_500) } }}
-                cardContainerClass={classes.cardMargin} features={{ features: ['Downloadable resources', 'All quizzes & exercises', '1-on-1 mentor sessions'], style: { color: themed(INDIGO_300, INDIGO_500) } }}
-                buttonColor={themed(INDIGO_300, INDIGO_500)} />
+            <Card title={{ label: 'Basic', style: { backgroundColor: this.state.primaryColor } }} price={{ label: '$25', style: { color: this.state.primaryColor } }}
+                cardContainerClass={classes.cardMargin} features={{ features: ['Online access to resources', 'Limited quizzes & exercises', 'Mentors for doubt clearance'], style: { color: this.state.primaryColor } }}
+                buttonColor={this.state.primaryColor} />
+            <Card title={{ label: 'Premium', style: { backgroundColor: this.state.secondaryColor } }} subtitle={{ label: 'Most Popular', style: { color: this.state.secondaryColor } }}
+                price={{ label: '$40', style: { color: this.state.secondaryColor } }} cardContainerClass={`${classes.cardMargin} ${classes.centerCard}`} buttonColor={this.state.secondaryColor}
+                features={{ features: ['Downloadable resources', '100+ quizzes & exercises', '1-on-1 mentor sessions'], style: { color: this.state.secondaryColor } }} />
+            <Card title={{ label: 'Ultimate', style: { backgroundColor: this.state.primaryColor } }} price={{ label: '$50', style: { color: this.state.primaryColor } }}
+                cardContainerClass={classes.cardMargin} features={{ features: ['Downloadable resources', 'All quizzes & exercises', '1-on-1 mentor sessions'], style: { color: this.state.primaryColor } }}
+                buttonColor={this.state.primaryColor} />
         </div>
     </>;
 }
