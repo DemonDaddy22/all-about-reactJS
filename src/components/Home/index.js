98ebe3a76cce185fb126dcf1c4b945be402c55a6
Rohan Gupta <guptarohan369@gmail.com>
2020-09-13 20:45:55 +0530

    add home header

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,5 +5,13 @@ import Page from '../../ui-components/Page';
 
 export default class Home extends React.Component {
 
-    render = () => <Page>Home</Page>;
+    render = () => <Page>
+        <div className={classes.homeContainer}>
+            <div className={classes.headerWrapper}>
+                <div className={classes.header}>
+                    <span className={classes.headerText}>All about React<div className={classes.underline}></div></span><span className={classes.headerHighlight}>JS</span>
+                </div>
+            </div>
+        </div>
+    </Page>;
 }
