4da68008d344a43fd02c15c749bcc83830f03642
Rohan Gupta <guptarohan369@gmail.com>
2020-09-17 23:07:53 +0530

    add inputRows and inputButtons

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import classes from './styles.module.css';
 import Page from '../../ui-components/Page';
+import rows from './rows';
 
 export default class Calculator extends React.Component {
 
     render = () => <Page>
-        <h1>Calculator</h1>
+        <div className={classes.calculatorContainer}>
+            {rows.map((row, index) => <InputRow />)}
+        </div>
     </Page>
 }
---
+++ src/components/Calculator/rows.js
@@ -0,0 +1,7 @@
+export const rows = [
+    [{ flex: 1, buttonContent: 'AC', position: 'top' }, { flex: 1, buttonContent: '+/-', position: 'top' }, { flex: 1, buttonContent: '%', position: 'top' }, { flex: 1, buttonContent: '÷', position: 'right' }],
+    [{ flex: 1, buttonContent: '7', position: 'center' }, { flex: 1, buttonContent: '8', position: 'center' }, { flex: 1, buttonContent: '9', position: 'center' }, { flex: 1, buttonContent: '×', position: 'right' }],
+    [{ flex: 1, buttonContent: '4', position: 'center' }, { flex: 1, buttonContent: '5', position: 'center' }, { flex: 1, buttonContent: '6', position: 'center' }, { flex: 1, buttonContent: '-', position: 'right' }],
+    [{ flex: 1, buttonContent: '1', position: 'center' }, { flex: 1, buttonContent: '2', position: 'center' }, { flex: 1, buttonContent: '3', position: 'center' }, { flex: 1, buttonContent: '+', position: 'right' }],
+    [{ flex: 2, buttonContent: '0', position: 'center' }, { flex: 1, buttonContent: '.', position: 'center' }, { flex: 1, buttonContent: '=', position: 'right' }],
+];
