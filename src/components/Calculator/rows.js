6d99970b533c1c4861d8fb9aace8a3f1a3f14a91
Rohan Gupta <guptarohan369@gmail.com>
2020-09-19 21:38:45 +0530

    add calculator wrapper styles

--- src/components/Calculator/rows.js
+++ src/components/Calculator/rows.js
@@ -3,5 +3,5 @@ export const rows = [
     [{ flex: 1, buttonContent: '7', position: 'center' }, { flex: 1, buttonContent: '8', position: 'center' }, { flex: 1, buttonContent: '9', position: 'center' }, { flex: 1, buttonContent: '×', position: 'right' }],
     [{ flex: 1, buttonContent: '4', position: 'center' }, { flex: 1, buttonContent: '5', position: 'center' }, { flex: 1, buttonContent: '6', position: 'center' }, { flex: 1, buttonContent: '-', position: 'right' }],
     [{ flex: 1, buttonContent: '1', position: 'center' }, { flex: 1, buttonContent: '2', position: 'center' }, { flex: 1, buttonContent: '3', position: 'center' }, { flex: 1, buttonContent: '+', position: 'right' }],
-    [{ flex: 2, buttonContent: '0', position: 'center' }, { flex: 1, buttonContent: '.', position: 'center' }, { flex: 1, buttonContent: '=', position: 'right' }],
+    [{ flex: '2 1 11%', buttonContent: '0', position: 'center' }, { flex: 1, buttonContent: '.', position: 'center' }, { flex: 1, buttonContent: '=', position: 'right' }],
 ];
