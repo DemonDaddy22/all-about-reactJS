4d89a164cfc068f2310379f95ec6214b33cce30b
Rohan Gupta <guptarohan369@gmail.com>
2020-09-23 21:25:01 +0530

    add colour util methods

--- src/utils/index.js
+++ src/utils/index.js
@@ -22,7 +22,7 @@ export const getPathValue = (obj, accumulatedPath, defaultValue) => {
 
 export const getColours = count => {
     let colours = [], i = 0;
-    
+
     while (i < count) {
         const colour = getRandomColor();
         if (!isColourPresent(colour, colours)) {
@@ -39,3 +39,53 @@ export const getRandomColor = () => `rgb(${getRandomValue(255)}, ${getRandomValu
 export const getRandomValue = value => Math.floor(Math.random() * value);
 
 export const isColourPresent = (colour, colours) => !isEmptyList(colours.filter(col => col === colour));
+
+export const isValidColour = colour => {
+    if (isEmptyString(colour)) return false;
+    const style = new Option().style;
+    style.color = colour;
+    return style.color === colour;
+}
+
+export const hexToRGB = colour => {
+    if (!isValidColour(colour)) return null;
+
+    let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i; // checks for 3 digit hex value (#0f0) and converts them to 6 digit hex value
+    colour = colour.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(colour);
+    return result ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})` : null;
+}
+
+export const rgbToHex = colour => {
+    if (!isValidColour(colour)) return null;
+
+    // Turn colour rgb(r, g, b) into rgb(r g b)
+    let sep = colour.indexOf(', ') > -1 ? ', ' : ' ';
+
+    // Turn colour rgb(r g b) into [r, g, b]
+    colour = colour.substr(4).split(')')[0].split(sep);
+
+    let r = parseInt(colour[0], 16), g = parseInt(colour[1], 16), b = parseInt(colour[2], 16);
+
+    if (r.length === 1) r = '0' + r;
+    if (g.length === 1) g = '0' + g;
+    if (b.length === 1) b = '0' + b;
+
+    return '#' + r + g + b;
+}
+
+export const isColourDark = colour => {
+    if (!isValidColour(colour)) return false;
+
+    if (!(/^#[0-9A-F]{3}{1,2}$/i.test(colour))) colour = rgbToHex(colour);
+    if (/^#[0-9A-F]{3}$/i.test(colour)) colour = colour.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => r + r + g + g + b + b);
+
+    let rgb = parseInt(colour, 16);   
+    let r = (rgb >> 16) & 0xff;  
+    let g = (rgb >> 8) & 0xff;  
+    let b = (rgb >> 0) & 0xff;
+
+    let luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+    return luma < 40;
+}
