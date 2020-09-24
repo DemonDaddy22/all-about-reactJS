0e846f04fbb0b97132b66c3f177617f3ac30e80c
Rohan Gupta <guptarohan369@gmail.com>
2020-09-24 23:36:29 +0530

    add color picker to projects list

--- src/components/Home/projects.js
+++ src/components/Home/projects.js
@@ -4,10 +4,13 @@ import pricingDarkImage from '../../images/pricing_dark.jpg';
 import pricingLightImage from '../../images/pricing_light.jpg';
 import calculatorDarkImage from '../../images/calculator_dark.jpg';
 import calculatorLightImage from '../../images/calculator_light.jpg';
+import pickerDarkImage from '../../images/picker_dark.jpg';
+import pickerLightImage from '../../images/picker_light.jpg';
 import { BASE_URL } from '../../resources/constants';
 
 export const projects = [
     { sequence: '1', title: 'Todos Manager', imageLight: todosLightImage, imageDark: todosDarkImage, projectPath: `${BASE_URL}/#/todos`, githubLink: 'https://github.com/DemonDaddy22/all-about-reactJS/tree/master/src/components/Todos' },
     { sequence: '2', title: 'Pricing Cards', imageLight: pricingLightImage, imageDark: pricingDarkImage, projectPath: `${BASE_URL}/#/pricing-cards`, githubLink: 'https://github.com/DemonDaddy22/all-about-reactJS/tree/master/src/components/PricingCards' },
     { sequence: '3', title: 'Calculator', imageLight: calculatorLightImage, imageDark: calculatorDarkImage, projectPath: `${BASE_URL}/#/calculator`, githubLink: 'https://github.com/DemonDaddy22/all-about-reactJS/tree/master/src/components/Calculator' },
+    { sequence: '4', title: 'Color Picker', imageLight: pickerLightImage, imageDark: pickerDarkImage, projectPath: `${BASE_URL}/#/color-picker`, githubLink: 'https://github.com/DemonDaddy22/all-about-reactJS/tree/master/src/components/ColorPicker' },
 ];
