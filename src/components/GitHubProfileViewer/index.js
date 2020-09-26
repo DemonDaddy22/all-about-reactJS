a0681047bd00e59a84980a1ac37fb83ac0068ce4
Rohan Gupta <guptarohan369@gmail.com>
2020-09-26 21:44:01 +0530

    add and test github APIs

--- src/components/GitHubProfileViewer/index.js
+++ src/components/GitHubProfileViewer/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { GITHUB_API_BASE } from '../../resources/constants';
 import Page from '../../ui-components/Page';
 
 import classes from './styles.module.css';
 
 export default class GitHubProfileViewer extends React.Component {
+
+    componentDidMount = () => {
+        const user = 'DemonDaddy22';
+        this.fetchUserData(user);
+        this.fetchReposData(user);
+    }
+
+    fetchUserData = user => {
+        fetch(GITHUB_API_BASE + `/users/${user}`)
+        .then(res => res.json())
+        .then(data => console.table(data));
+    }
+
+    fetchReposData = user => {
+        fetch(GITHUB_API_BASE + `/users/${user}/repos`)
+        .then(res => res.json())
+        .then(data => console.log(data));
+    }
 
     render = () => {
 
--- src/resources/constants.js
+++ src/resources/constants.js
@@ -1,2 +1,3 @@
 export const GITHUB_BASE = 'https://github.com/DemonDaddy22';
 export const BASE_URL = process.env.NODE_ENV === 'production' ? '/all-about-reactJS' : '';
+export const GITHUB_API_BASE = 'https://api.github.com';
