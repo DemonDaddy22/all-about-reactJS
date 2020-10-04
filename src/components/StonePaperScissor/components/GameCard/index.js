49e8ae6c1660331c3210eb03728e96e8f5835e86
Rohan Gupta <guptarohan369@gmail.com>
2020-10-04 23:08:53 +0530

    add svg color globally

--- src/components/StonePaperScissor/components/GameCard/index.js
+++ src/components/StonePaperScissor/components/GameCard/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 import classes from './styles.module.css';
 
-const GameCard = React.memo(({option, style}) => {
+const GameCard = React.memo(({option, cardStyle}) => {
 
-    return <div style={style} className={classes.card}></div>
+    return <div style={cardStyle} className={classes.card}>
+        <div className={classes.cardImageWrapper}>
+            <img src={option.image} alt={option.title} className={classes.cardImage} />
+        </div>
+        <div className={classes.cardTitle}>{option.title}</div>
+    </div>
 });
 
 export default GameCard;
--- src/components/StonePaperScissor/components/GameView/index.js
+++ src/components/StonePaperScissor/components/GameView/index.js
@@ -7,7 +7,7 @@ const GameView = React.memo(({ options, selectOption }) => {
 
     return <>
         <div className={classes.cardsWrapper}>
-            {Object.keys(options).map(option => <GameCard style={{ cursor: 'pointer' }} option={option} />)}
+            {Object.keys(options).map(option => <GameCard cardStyle={{ cursor: 'pointer' }} option={options[option]} />)}
         </div>
     </>;
 });
