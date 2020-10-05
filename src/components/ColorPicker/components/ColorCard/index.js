ec249723de42a1c9ac345c3b8fb0907e70ed0255
Jaikishann <jaikishan.seshagiri@anywhere.co>
2020-10-05 12:21:50 +0530

    showing copy colour code icon on hover

--- src/components/ColorPicker/components/ColorCard/index.js
+++ src/components/ColorPicker/components/ColorCard/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import copy from 'copy-to-clipboard';
 import classes from './styles.module.css';
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
 
 export default class ColorCard extends React.Component {
 
+    onClickCopy = (e) => {
+        e.stopPropagation();
+        copy(this.props.colour);
+    }
+
     render = () => {
 
-        return <div className={classes.card} style={{ backgroundColor: this.props.colour }} onClick={() => this.props.handleColourChange(this.props.colour)}></div>;
+        return <div className={classes.card} style={{ backgroundColor: this.props.colour }} onClick={() => this.props.handleColourChange(this.props.colour)}>
+            <div className={classes.copy}>
+                <span className={classes.code}>{this.props.colour}</span>
+                <div className={classes.icon} onClick={this.onClickCopy}>
+                    <FileCopyOutlinedIcon fontSize='small' />
+                </div>
+            </div>
+        </div>;
     }
 }
