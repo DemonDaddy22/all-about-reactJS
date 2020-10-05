9a6316835265ef60314d6b57a8aa58f19d379109
Rohan Gupta <guptarohan369@gmail.com>
2020-10-05 17:01:40 +0530

    add comments for reference

--- src/components/StonePaperScissor/components/ResultView/index.js
+++ src/components/StonePaperScissor/components/ResultView/index.js
@@ -5,6 +5,9 @@ import classes from './styles.module.css';
 const ResultView = React.memo(() => {
 
     // console log to check if component renders if props aren't changed
+    // change border colour of cards based on result
+    // display result text
+    // get random option from set of options
     return <></>;
 });
 
