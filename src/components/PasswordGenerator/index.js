3a75891f8494d99327cf99f09c63bace8d562ff9
Rohan Gupta <guptarohan369@gmail.com>
2020-10-07 23:15:14 +0530

    add basic structure and styling

--- src/components/PasswordGenerator/index.js
+++ src/components/PasswordGenerator/index.js
@@ -5,10 +5,23 @@ import Page from '../../ui-components/Page';
 
 export default class PasswordGenerator extends React.Component {
 
+    state = {
+        passwordString: 'Test'
+    }
+
     render = () => {
 
         return <Page>
-
+            <div className={classes.generatorWrapper}>
+                <div className={classes.generatorContainer}>
+                    <div className={classes.passwordContainer}>
+                        {this.state.passwordString}
+                    </div>
+                    <div className={classes.optionsContainer}>
+                        
+                    </div>
+                </div>
+            </div>
         </Page>
     }
 }
