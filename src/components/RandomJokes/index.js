6f87986be84d5225477aef37e280e206e6410dc2
Rohan Gupta <guptarohan369@gmail.com>
2020-10-17 23:25:52 +0530

    add buttons

--- src/components/RandomJokes/index.js
+++ src/components/RandomJokes/index.js
@@ -4,6 +4,7 @@ import classes from './styles.module.css';
 import { copyTextToClipboard, handleError, isEmptyObject, isEmptyString } from '../../utils';
 import Page from '../../ui-components/Page';
 import SpinnerLoader from '../../ui-components/SpinnerLoader';
+import Button from '../../ui-components/Button';
 import { Tab, Tabs } from '@material-ui/core';
 import AllJokes from './components/AllJokes';
 import LikedJokes from './components/LikedJokes';
@@ -13,10 +14,11 @@ export default class RandomJokes extends React.Component {
     state = {
         jokes: [],
         currentTab: 0,
-        loader: true
+        loader: true,
+        showButton: false
     }
 
-    componentDidMount = () => this.fetchJokes(); 
+    componentDidMount = () => this.fetchJokes();
 
     fetchJokes = (numJokes = 10) => this.setState({ loader: true }, async () => {
         let jokes = [];
@@ -30,7 +32,7 @@ export default class RandomJokes extends React.Component {
             }
         }
         this.setState({ loader: false, jokes: [...this.state.jokes, ...jokes] }, () => {
-            jokes.forEach((joke, index) => setTimeout(() => this.toggleJokeVisibility(joke.id, true), index * 50))
+            jokes.forEach((joke, index) => setTimeout(() => this.toggleJokeVisibility(joke.id, true, this.handleShowButton), index * 50))
         });
     });
 
@@ -60,11 +62,15 @@ export default class RandomJokes extends React.Component {
         jokes: prevState.jokes.map(joke => joke.id === id ? { ...joke, liked: !joke.liked } : joke)
     }));
 
-    onTabChange = (e, tab) => this.setState({ currentTab: tab });
+    handleShowButton = () => setTimeout(() => this.setState({ showButton: true }), 750);
+
+    goToTop = () => window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+
+    onTabChange = (e, tab) => this.setState({ currentTab: tab, showButton: tab === 0 });
 
     renderContent = tab => {
         switch (tab) {
-            case 0:
+            case 0: default:
                 return <AllJokes jokes={this.state.jokes} loader={this.state.loader}
                     handleCopyJoke={this.handleCopyJoke} handleLikeJoke={this.handleLikeJoke} />
             case 1:
@@ -74,7 +80,7 @@ export default class RandomJokes extends React.Component {
     }
 
     render = () => {
-        const { currentTab, loader } = this.state;
+        const { currentTab, loader, showButton } = this.state;
 
         return <Page>
             <>
@@ -84,6 +90,11 @@ export default class RandomJokes extends React.Component {
                     <Tab label='Liked Jokes' classes={{ root: classes.tabRoot }} disableFocusRipple disableRipple disableTouchRipple></Tab>
                 </Tabs>
                 {this.renderContent(currentTab)}
+                {showButton && <div className={classes.btnWrapper}>
+                    <Button backgroundColor='var(--secondary-theme-color)' onClick={() => this.fetchJokes()} labelStyles={{ display: 'inline-flex', padding: '0 1rem' }}>Get more jokes</Button>
+                    <Button variant='outlined' borderColor='var(--secondary-theme-color)' labelColor='var(--secondary-theme-color)' backgroundColor='var(--secondary-theme-color)'
+                        className={classes.scrollToTopBtn} onClick={() => this.goToTop()} labelStyles={{ display: 'inline-flex', padding: '0 1rem' }}>Go to top</Button>
+                </div>}
             </>
         </Page>;
     }
