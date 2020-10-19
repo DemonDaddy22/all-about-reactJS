3a2baec4cb5b383ad725e2a4c0a1cccf95da0a63
Rohan Gupta <guptarohan369@gmail.com>
2020-10-20 00:17:21 +0530

    add background color and box-shadow on hovering cell

--- src/components/HoverBoard/components/BoardCell/index.js
+++ src/components/HoverBoard/components/BoardCell/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import { BLUE_400, BLUE_600, GREEN_400, GREEN_600, ORANGE_400, ORANGE_600, PURPLE_400, PURPLE_600, RED_400, RED_600, YELLOW_400, YELLOW_600 } from '../../../../resources/colors';
+import { themed } from '../../../../utils/theme';
 
 import classes from './styles.module.css';
+
+const COLORS = Object.freeze({
+    RED: themed(RED_600, RED_400),
+    PURPLE: themed(PURPLE_600, PURPLE_400),
+    BLUE: themed(BLUE_600, BLUE_400),
+    GREEN: themed(GREEN_600, GREEN_400),
+    ORANGE: themed(ORANGE_600, ORANGE_400),
+    YELLOW: themed(YELLOW_600, YELLOW_400)
+});
+
+const getColor = () => {
+    const color = Object.keys(COLORS)[Math.floor(Object.keys(COLORS).length * Math.random())];
+    return COLORS[color];
+}
 
 export default class BoardCell extends React.Component {
 
@@ -8,7 +24,13 @@ export default class BoardCell extends React.Component {
         isHovering: false
     }
 
-    toggleHovering = hover => console.log(hover);
+    toggleHovering = hover => this.setState({ isHovering: hover });
 
-    render = () => <div onMouseEnter={() => this.toggleHovering(true)} onMouseLeave={() => this.toggleHovering(false)} className={classes.cell}></div>
+    render = () => {
+        const color = getColor();
+        const style = { backgroundColor: this.state.isHovering ? color : 'var(--card-bg)', boxShadow: this.state.isHovering ? `0 0 2px ${color}, 0 0 8px ${color}` : '0 0 2px var(--tag-color)' };
+
+        return <div onMouseEnter={() => this.toggleHovering(true)} onMouseLeave={() => this.toggleHovering(false)}
+            className={classes.cell} style={style}></div>
+    }
 }
--- src/components/HoverBoard/index.js
+++ src/components/HoverBoard/index.js
@@ -4,16 +4,17 @@ import classes from './styles.module.css';
 import Page from '../../ui-components/Page';
 import BoardCell from './components/BoardCell';
 
-const BOARD_SIZE = 150;
+const BOARD_SIZE = 300;
 
 export default class HoverBoard extends React.Component {
 
-    render = () => {
+    updateComponent = (refresher = null) => refresher && this.setState({ refresher });
 
+    render = () => {
         let cells = [];
         for (let i = 0; i < BOARD_SIZE; i++) cells.push(<BoardCell key={`cell-${i}`} />);
 
-        return <Page>
+        return <Page shouldComponentUpdate={this.updateComponent}>
             <div className={classes.boardWrapper}>
                 <div className={classes.board}>
                     {cells}
