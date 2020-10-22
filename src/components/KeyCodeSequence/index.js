01d1afa7073692e5e662b6a1cf150a4544c03021
Rohan Gupta <guptarohan369@gmail.com>
2020-10-22 21:34:44 +0530

    add keydown event to track key press events

--- src/components/KeyCodeSequence/index.js
+++ src/components/KeyCodeSequence/index.js
@@ -5,8 +5,36 @@ import Page from '../../ui-components/Page';
 
 export default class KeyCodeSequence extends React.Component {
 
+    // show an option to list of all the options
+
+    state = {
+        eventInfo: null
+    }
+
+    componentDidMount = () => {
+        window.addEventListener('keydown', this.handleKeyPress);
+    }
+
+    componentWillUnmount = () => {
+        window.removeEventListener('keydown', this.handleKeyPress);
+    }
+
+    handleKeyPress = e => {
+        this.setState({ eventInfo: e });
+    };
+    // e.code - 'KeyK'
+    // e.key - 'k'
+    // e.keyCode - 75
+    // which - 75 (deprecated)
+
     render = () => {
 
-        return <Page>KeyCodeSequence</Page>;
+        return <Page>
+            <div className={classes.sequenceWrapper}>
+                {!this.state.eventInfo ?
+                    <div className={classes.noData}>Press a key to get details of the pressed key</div> :
+                    <></>}
+            </div>
+        </Page>;
     }
 }
