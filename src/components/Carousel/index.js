be3293c7dbecb6f72e30dfa9280ffedae349733e
Rohan Gupta <guptarohan369@gmail.com>
2020-11-01 23:57:04 +0530

    add 6th image and decrease carousel height on small screens

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -10,7 +10,8 @@ const images = ['https://images.unsplash.com/photo-1432847712612-926caafaa802?ix
     'https://images.unsplash.com/photo-1513151233558-d860c5398176?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80',
     'https://images.unsplash.com/photo-1488229297570-58520851e868?ixlib=rb-1.2.1&auto=format&fit=crop&w=2849&q=80',
     'https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80',
-    'https://images.unsplash.com/photo-1535478044878-3ed83d5456ef?ixlib=rb-1.2.1&auto=format&fit=crop&w=2869&q=80'];
+    'https://images.unsplash.com/photo-1535478044878-3ed83d5456ef?ixlib=rb-1.2.1&auto=format&fit=crop&w=2869&q=80',
+    'https://images.unsplash.com/photo-1484268234627-2278797bec04?ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80'];
 
 export default class Carousel extends React.Component {
 
