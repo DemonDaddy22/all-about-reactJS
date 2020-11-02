7b352a64e8828108dde31f65301cdbd3fea29361
Rohan Gupta <guptarohan369@gmail.com>
2020-11-02 22:40:46 +0530

    add themealdb api

--- src/components/MealGenerator/index.js
+++ src/components/MealGenerator/index.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 import classes from './styles.module.css';
 import Page from '../../ui-components/Page';
+import { handleError } from '../../utils';
 
 export default class MealGenerator extends React.Component {
+
+    state = {
+        loader: true,
+        mealData: null
+    }
+
+    componentDidMount = () => {
+        this.fetchMealData();
+    }
+
+    fetchMealData = () => this.setState({ loader: true }, () => fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(handleError)
+        .then(res => res.json())
+        .then(data => this.setState({ loader: false, mealData: data, snack: null }, () => console.log(data)))
+        .catch(error => this.setState({
+            loader: false, mealData: null, snack: {
+                open: true,
+                message: error.message,
+                severity: 'error'
+            }
+        })));
 
     render = () => {
 
