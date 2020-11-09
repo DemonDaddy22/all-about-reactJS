d21d0021ac21515dd0048b89c1bb84dcb89cba1f
Rohan Gupta <guptarohan369@gmail.com>
2020-11-09 23:36:53 +0530

    Add git message template Problem Solution * add commit message template globally Note - in case of emergency, bypass the template using git commit -m 'message'

--- src/components/BillGenerator/components/Bill/index.js
+++ src/components/BillGenerator/components/Bill/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 import classes from './styles.module.css';
 
-const Bill = React.memo(({}) => {
-    return <>
-        <div className={classes.title}>Order Summary</div>
-        <div className={classes.noData}>You haven't selected any item yet :(</div>
-    </>;
-});
+const Bill = React.memo(({ }) => <>
+    <div className={classes.title}>Order Summary</div>
+    <div className={classes.noData}>You haven't selected any item yet :(</div>
+</>
+);
 
 export default Bill;
