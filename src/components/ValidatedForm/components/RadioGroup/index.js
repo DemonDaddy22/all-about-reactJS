5da1d07337c3f3ba74be503e5fb876083933179c
Rohan Gupta <guptarohan369@gmail.com>
2020-11-17 23:53:49 +0530

    update radio and radio group styles

--- src/components/ValidatedForm/components/RadioGroup/index.js
+++ src/components/ValidatedForm/components/RadioGroup/index.js
@@ -7,7 +7,7 @@ import RadioButton from '../../../../ui-components/Radio';
 const RadioGroup = React.memo(({ options, selectedOption, onChange, header, className }) => <div className={`${className} ${classes.radioGroupWrapper}`}>
     {header && <div className={classes.header}>{header}</div>}
     {!isEmptyList(options) && <div className={classes.radioGroup}>
-        {options.map(option => <RadioButton label={option.label} checked={option.id === selectedOption.id} onchange={() => onChange(option)} />)}    
+        {options.map(option => <RadioButton label={option.label} checked={option.id === selectedOption.id} onChange={() => onChange(option)} />)}    
     </div>}
 </div>);
 
--- src/ui-components/Radio/index.js
+++ src/ui-components/Radio/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { FormControlLabel, withStyles } from '@material-ui/core';
 import Radio from '@material-ui/core/Radio';
 import { THEME_COLOR } from '../../resources/colors';
+import classes from './styles.module.css';
 
 const styles = {
     root: props => ({
@@ -21,7 +22,7 @@ class RadioButton extends React.Component {
     render = () => {
         const { checked, disabled, label, labelProps } = this.props;
 
-        return <FormControlLabel label={label} control={
+        return <FormControlLabel classes={{ label: classes.label }} label={label} control={
             <Radio color='default' checked={checked} disabled={disabled} {...this.props} />
         } {...labelProps}></FormControlLabel>
     }
