5d0c6dfce56ba8586823d47f15665f067efb5569
Rohan Gupta <guptarohan369@gmail.com>
2020-11-29 01:59:16 +0530

    add inputs and pokemon api, also update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,25 +19,46 @@ module.exports = {
     "rules": {
         "comma-dangle": 0,
         "react/jsx-uses-vars": 1,
-        "no-unused-vars": "warn",
-        "no-unexpected-multiline": "warn"
+        "no-unused-vars": 1,
+        "no-unexpected-multiline": 1,
+        "no-debugger": 1,
+        "no-alert": 0,
+        "no-await-in-loop": 1,
+        "no-return-assign": ["error", "except-parens"],
+        "no-unused-expressions": [
+            2,
+            {
+                "allowTaggedTemplates": true,
+            },
+        ],
+        "import/prefer-default-export": 0,
+        "import": 0,
+        "react/require-default-props": 0,
+        "quotes": [
+            2,
+            "single",
+            {
+                "avoidEscape": true,
+                "allowTemplateLiterals": true,
+            },
+        ],
     },
     "settings": {
         "react": {
-          "createClass": "createReactClass",
-          "pragma": "React",
-          "fragment": "Fragment",
-          "version": "detect",
-          "flowVersion": "0.53"
+            "createClass": "createReactClass",
+            "pragma": "React",
+            "fragment": "Fragment",
+            "version": "detect",
+            "flowVersion": "0.53"
         },
         "propWrapperFunctions": [
             "forbidExtraProps",
-            {"property": "freeze", "object": "Object"},
-            {"property": "myFavoriteWrapper"}
+            { "property": "freeze", "object": "Object" },
+            { "property": "myFavoriteWrapper" }
         ],
         "linkComponents": [
-          "Hyperlink",
-          {"name": "Link", "linkAttribute": "to"}
+            "Hyperlink",
+            { "name": "Link", "linkAttribute": "to" }
         ]
     }
 };
--- src/components/Pokedex/index.js
+++ src/components/Pokedex/index.js
@@ -2,21 +2,73 @@ import React from 'react';
 
 import classes from './styles.module.css';
 import Page from '../../ui-components/Page';
+import DoneOutlineRoundedIcon from '@material-ui/icons/DoneOutlineRounded';
+import { InputAdornment } from '@material-ui/core';
+import Input from '../../ui-components/Input';
+import Iconbutton from '../../ui-components/Button/Iconbutton';
+import { handleError, isEmptyString } from '../../utils';
 
 export default class Pokedex extends React.Component {
 
     state = {
-        loader: true
+        loader: true,
+        index: 1,
+        name: '',
+        pokemonData: null
     }
 
     updateComponent = (refresher = null) => refresher && this.setState({ refresher });
-    
+
+    fetchPokemonData = pokemon => this.setState({ loader: true }, () => fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        .then(handleError)
+        .then(res => res.json())
+        .then(data => this.setState({ loader: false, pokemonData: data, snack: null }))
+        .catch(error => this.setState({
+            loader: false, pokemonData: null, snack: {
+                open: true,
+                message: error.message,
+                severity: 'error'
+            }
+        })));
+
+    // add error handling for indices < 1 and empty strings
+    handleValueChange = (e, key) => {
+        if (isEmptyString(key)) return;
+        const value = e.target.value;
+        this.setState({ [key]: value });
+    }
+
+    handleInputSubmit = key => {
+        if (isEmptyString(key)) return;
+        const value = this.state[key], initValue = key === 'index' ? 0 : '';
+        this.setState({ [key]: initValue }, () => this.fetchPokemonData(value));
+    }
+
     render = () => {
+        const { index, name } = this.state;
 
         return <Page shouldComponentUpdate={this.updateComponent}>
             <div className={classes.title}>Pokédex</div>
-            <div className={classes.inputWrapper}>
+            <div className={classes.inputContainer}>
                 <div className={classes.label}>Search <span className={classes.pokemonTitle}>Pokémon</span> by</div>
+                <div className={classes.inputWrapper}>
+                    <div className={classes.searchInput}>
+                        <Input autoComplete='off' id='index' type='number' label='Index' onChange={e => this.handleValueChange(e, 'index')}
+                            value={index} variant='outlined' rootInputStyles={{ color: 'var(--text)', borderRadius: 'inherit' }}
+                            InputProps={{
+                                endAdornment: <InputAdornment position='end'>
+                                    <Iconbutton iconColor='var(--text)' onClick={() => this.handleInputSubmit('index')} icon={<DoneOutlineRoundedIcon />}></Iconbutton></InputAdornment>
+                            }} />
+                    </div>
+                    <div className={classes.searchInput}>
+                        <Input autoComplete='off' id='name' label='Name' onChange={e => this.handleValueChange(e, 'name')}
+                            value={name} variant='outlined' rootInputStyles={{ color: 'var(--text)', borderRadius: 'inherit' }}
+                            InputProps={{
+                                endAdornment: <InputAdornment position='end'>
+                                    <Iconbutton iconColor='var(--text)' onClick={() => this.handleInputSubmit('name')} icon={<DoneOutlineRoundedIcon />}></Iconbutton></InputAdornment>
+                            }} />
+                    </div>
+                </div>
             </div>
         </Page>
     }
