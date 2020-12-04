c9600c57bdb0ee24408759e972d3aff1a16300a6
Rohan Gupta <guptarohan369@gmail.com>
2020-12-05 02:12:26 +0530

    add styling and tweet block title content

--- src/components/TwitterUIClone/components/TweetBlock/index.js
+++ src/components/TwitterUIClone/components/TweetBlock/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 import classes from './styles.module.css';
+import { getPathValue } from '../../../../utils';
+import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRounded';
 
-const TweetBlock = React.memo(({ tweet }) => <div className={classes.tweet}>
-    {tweet.content}
-</div>);
+const TweetBlock = React.memo(({ tweet, blockBackground, color, secondaryColor, borderColor }) =>
+    <div className={classes.tweet} style={{ backgroundColor: blockBackground, borderColor: borderColor }}>
+        <div className={classes.column}>
+            <img src={getPathValue(tweet, 'profile_pic', '')} alt={tweet.username} className={classes.profilePic}></img>
+        </div>
+        <div className={classes.column}>
+            <div className={classes.titleWrapper}>
+                <div style={{ color: color }} className={classes.name}>{getPathValue(tweet, 'name', '')}</div>
+                {tweet?.is_verified && <div style={{ color: color }} className={classes.verified}><CheckCircleRoundedIcon fontSize='inherit' color='inherit' /></div>}
+                <div style={{ color: secondaryColor }} className={classes.username}>@{getPathValue(tweet, 'username', '')}</div>
+                <div style={{ color: secondaryColor }} className={classes.divider}>·</div>
+                <div style={{ color: secondaryColor }} className={classes.time}>{getPathValue(tweet, 'post_time', '')}</div>
+            </div>
+        </div>
+    </div>);
 
 export default TweetBlock;
--- src/components/TwitterUIClone/index.js
+++ src/components/TwitterUIClone/index.js
@@ -4,14 +4,19 @@ import classes from './styles.module.css';
 import { tweets } from './tweets';
 import Page from '../../ui-components/Page';
 import TweetBlock from './components/TweetBlock';
+import { themed } from '../../utils/theme';
 
 export default class TwitterUIClone extends React.Component {
 
+    updateComponent = (refresher = null) => refresher && this.setState({ refresher });
+
     render = () => {
 
-        return <Page>
-            <div className={classes.container}>
-                {tweets.map((tweet, index) => <TweetBlock key={`tweet-${index}`} tweet={tweet} />)}
+        return <Page shouldComponentUpdate={this.updateComponent}>
+            <div style={{ borderColor: themed('#546873', '#37454d') }} className={classes.container}>
+                {tweets.map((tweet, index) => <TweetBlock key={`tweet-${index}`} tweet={tweet}
+                    blockBackground={themed('#fff', '#15212b')} color={themed('#000', '#fff')}
+                    secondaryColor={themed('#546873', '#8899a6')} borderColor={themed('#546873', '#37454d')} />)}
             </div>
         </Page>;
     }
